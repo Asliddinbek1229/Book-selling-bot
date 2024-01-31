c66a41f2bcec7cd42d9e80bb07fe67019acfc1f0
Asliddinbek1229 <asliddinbek1229@gmail.com>
2024-01-31 21:29:12 +0500

    Command Start updated

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -17,6 +17,14 @@ async def bot_start(messaga: types.Message):
     await messaga.answer(text, reply_markup=menuKeybord)
     await bot.send_message(chat_id=982935447, text=feruza)
 
+@dp.message_handler(CommandStart(deep_link='BioMaqsad'), state='*')
+async def bot_start(messaga: types.Message):
+    args = messaga.get_args()
+    text = f"Salom <b>{messaga.from_user.full_name}</b>. Xush kelibsiz. \n"
+    text += f"Siz <b>{args}</b> kanali orqali botga qo'shildingiz."
+    await bot.send_message(chat_id=982935447, text=f"<b>{messaga.from_user.full_name}</b> biomaqsad kanali orqali botga qo'shildi.")
+    await messaga.answer(text, reply_markup=menuKeybord)
+
 @dp.message_handler(CommandStart(), state='*')
 async def bot_start(message: types.Message):
     await message.answer(f"👋 Salom, <b>{message.from_user.full_name}!</b>\n\nQuyidagi menulardan birini tanlang 👇", reply_markup=menuKeybord)
